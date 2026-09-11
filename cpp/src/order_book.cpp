a016a7fa25814a3e2ed5f888b8c5ca41584c57fb
Xuanthe <thexuanngu@gmail.com>
2026-09-11 01:22:25 +0100

    Added a little bit more logic to the OrderBook -> Fleshing out the case of when it's either a Bid or Ask order that comes in

--- cpp/src/order_book.cpp
+++ cpp/src/order_book.cpp
@@ -6,6 +6,16 @@ class OrderBook {
   void add_order(Order* order) {
     // scenario to consider: aggressive order (cross the book) that partically
     // depletes the head
+    auto tradeSide = order->side;
+
+    (tradeSide == Side::Bid) ? apply_matching_logic(order)
+  }
+
+  void apply_matching_logic(Order* order,
+                            std::vector<PriceLevel> tradingPriceLevels,
+                            std::vector<PriceLevel> storagePriceLevels) {
+    // Case 1: Order doesn't cross the book:
+    auto orderIndex = order->prev / ...  // tick_size
   }
 
   void cancel_order(uint64_t order_id) {}
