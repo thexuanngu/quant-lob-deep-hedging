884dd32959684f1683587994c6c8561af7a4a0de
Xuanthe <xnguyen@college.harvard.edu>
2026-09-24 01:23:32 +0100

    Minor tweak -> variable declaration inside case body must be wrapped inside curly braces

--- cpp/src/engine.cpp
+++ cpp/src/engine.cpp
@@ -6,19 +6,20 @@ namespace lob {
 void LOBEngine::runEngine() {
   while (running_.load(std::memory_order_acquire)) {
     auto event_opt = ring_buffer_.try_pop();
-    if (!event_opt) std::this_thread::yield();  // Spin-lock: immediately retry -> Is this ok?
+    if (!event_opt)
+      std::this_thread::yield();  // Spin-lock: immediately retry -> Is this ok?
 
     OrderEvent& ev = *event_opt;
 
     // Route the event
     switch (ev.action) {
-      case Action::Add:
+      case Action::Add: {
         Order* order =
             order_pool_.allocate(ev.order_id, ev.price, ev.qty, ev.side);
         if (order) {  // null check
           book_.add_order(order);
         }
-        break;
+      } break;
       case Action::Cancel:
         book_.cancel_order(ev.order_id);
         break;
